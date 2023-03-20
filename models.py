b03eb494f3205b7835c164668bb7515530f7fcf9
DannyHo1209 <113899395+DannyHo1209@users.noreply.github.com>
2023-03-20 14:53:13 +0800

    User models cteated

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, DATE
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "Users"
+    id = Column(INTEGER, primary_key=True, index=True)
+    name = Column(VARCHAR(50), nullable=False)
+    gender = Column(VARCHAR(1), nullable=False)
+    birth_date = Column(DATE, nullable=False)
+    password = Column(VARCHAR(50), nullable=False)
