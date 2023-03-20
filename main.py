13cf91018094614795a08797efee3431252fb32e
Sally Lin <LS0723000@gmail.com>
2023-03-20 17:18:42 +0800

    add user router to main

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
+from database import engine
+from routers import user
+
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(user.router)
