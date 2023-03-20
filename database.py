13c6968ec49b19fe1c1f9fe36fb9dbc1d63fc545
Sally Lin <LS0723000@gmail.com>
2023-03-20 14:07:31 +0800

    database config & ssh connected

---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sshtunnel import SSHTunnelForwarder
+
+server = SSHTunnelForwarder(
+    ('122.116.234.117', 22),
+    ssh_username='ubuntu',
+    ssh_password='ubuntu server',
+    remote_bind_address=('localhost', 3306)
+)
+
+server.start()
+local_port = str(server.local_bind_port)
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://user:rKelsaoUa@localhost:{local_port}/test_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
