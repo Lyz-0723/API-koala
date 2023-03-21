8e8db7f135c742f0c6b58bb85992bf401e689e20
DannyHo1209 <113899395+DannyHo1209@users.noreply.github.com>
2023-03-21 23:35:20 +0800

    aysnc database model recreate

--- models.py
+++ models.py
@@ -1,26 +1,26 @@
+import sqlalchemy
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, CHAR, DATE, DATETIME
 from sqlalchemy.orm import relationship
-from database import Base
+from database import metadata
 
 
-class User(Base):
-    __tablename__ = "Users"
-    id = Column(INTEGER, primary_key=True, index=True)
-    name = Column(VARCHAR(50), nullable=False)
-    gender = Column(VARCHAR(1), nullable=False)
-    birth_date = Column(DATE, nullable=False)
-    password = Column(CHAR(60), nullable=False)
+User = sqlalchemy.Table(
+    "Users",
+    metadata,
+    Column("id", sqlalchemy.INTEGER, primary_key=True, index=True),
+    Column("name", sqlalchemy.VARCHAR(50), nullable=False),
+    Column("gender", sqlalchemy.VARCHAR(1), nullable=False),
+    Column("birth_date", sqlalchemy.DATE, nullable=False),
+    Column("password", sqlalchemy.CHAR(60), nullable=False),
+)
 
-    articles = relationship("Article", back_populates="creator")
-
-
-class Article(Base):
-    __tablename__ = "Articles"
-    id = Column(INTEGER, primary_key=True, index=True)
-    title = Column(VARCHAR(50), nullable=False)
-    body = Column(VARCHAR(600), nullable=False)
-    created_time = Column(DATETIME, nullable=False)
-    creator_id = Column(INTEGER, ForeignKey("Users.id"))
-
-    creator = relationship("User", back_populates="articles")
+Article = sqlalchemy.Table(
+    "Article",
+    metadata,
+    Column("id", sqlalchemy.INTEGER, primary_key=True, index=True),
+    Column("title", sqlalchemy.VARCHAR(50), nullable=False),
+    Column("body", sqlalchemy.VARCHAR(600), nullable=False),
+    Column("created_time", sqlalchemy.DATETIME, nullable=False),
+    Column("creator_id", INTEGER, ForeignKey("Users.id"))
+)
