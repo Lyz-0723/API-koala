1e66ee06671b331e21b3701f77ec00bac8acb0bd
DannyHo1209 <113899395+DannyHo1209@users.noreply.github.com>
2023-03-22 00:29:12 +0800

    Article CRUD refactor

--- repositories/ArticleCRUD.py
+++ repositories/ArticleCRUD.py
@@ -1,30 +1,30 @@
 import datetime
 
-from sqlalchemy.orm import Session
-import models
+from models import Article
+from database import database
 import schemas
 
 
-def get_all_articles(db: Session):
-    return db.query(models.Article).all()
+async def get_all_articles():
+    query = Article.select()
+    return await database.fetch_all(query)
 
 
-def get_specific_article(article_id: int, db: Session):
-    return db.query(models.Article).filter_by(id=article_id).first()
+async def get_specific_article(article_id: int):
+    query = Article.select().where(Article.c.id == article_id)
+    return await database.fetch_one(query)
 
 
-def create_article(article: schemas.ArticleBase, creator: schemas.User, db: Session):
-    new_article = models.Article(title=article.title,
-                                 body=article.body,
-                                 created_time=datetime.datetime.now(),
-                                 creator_id=creator.id)
-    db.add(new_article)
-    db.commit()
-    db.refresh(new_article)
-    return new_article
+async def create_article(article: schemas.ArticleBase, creator: schemas.User):
+    query = Article.insert().values(title=article.title,
+                                    body=article.body,
+                                    created_time=datetime.datetime.now(),
+                                    creator_id=creator.id)
+    await database.execute(query)
+    return article
 
 
-def delete_article(article_id: int, db: Session):
-    db.query(models.Article).filter_by(id=article_id).delete(synchronize_session=False)
-    db.commit()
+async def delete_article(article_id: int):
+    query = Article.select().where(Article.c.id == article_id)
+    await database.execute(query)
     return {"Article deletion done"}
--- routers/article.py
+++ routers/article.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
 import database
+import models
 import schemas
 from authentication.JWTtoken import get_current_user
 from repositories import ArticleCRUD
@@ -15,29 +16,26 @@ router = APIRouter(
 
 
 # Getting the information of all articles
-@router.get("/")
-def get_all_articles(current_user: Annotated[schemas.User, Depends(get_current_user)],
-                     db: Session = Depends(database.get_db)) -> list[schemas.Article]:
-    return ArticleCRUD.get_all_articles(db)
+@router.get("/", response_model=models.Article)
+def get_all_articles(current_user: Annotated[schemas.User, Depends(get_current_user)]):
+    return ArticleCRUD.get_all_articles()
 
 
 # Creating an article
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=models.Article, status_code=status.HTTP_201_CREATED)
 def create_article(article: schemas.ArticleBase,
-                   current_user: Annotated[schemas.User, Depends(get_current_user)],
-                   db: Session = Depends(database.get_db)) -> schemas.CreateArticle:
-    return ArticleCRUD.create_article(article, current_user, db)
+                   current_user: Annotated[schemas.User, Depends(get_current_user)]):
+    return ArticleCRUD.create_article(article, current_user)
 
 
 # Deleting an article
 @router.delete("/{id}", status_code=status.HTTP_202_ACCEPTED)
 def delete_article(article_id: int,
-                   current_user: Annotated[schemas.User, Depends(get_current_user)],
-                   db: Session = Depends(database.get_db)):
-    if not ArticleCRUD.get_specific_article(article_id, db):
+                   current_user: Annotated[schemas.User, Depends(get_current_user)]):
+    if not ArticleCRUD.get_specific_article(article_id):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Article not found")
 
-    if not current_user.id == ArticleCRUD.get_specific_article(article_id, db).creator_id:
+    if not current_user.id == ArticleCRUD.get_specific_article(article_id).creator_id:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Article not belongs to the user")
 
-    return ArticleCRUD.delete_article(article_id, db)
+    return ArticleCRUD.delete_article(article_id)
